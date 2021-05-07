b1869965b207004009df7950fe67d586bc4d8a29
Andrew High <andrewhigh4@hotmail.com>
2021-05-07 13:55:35 +0100

    working mvp

---
+++ js/app.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const newWhiskyForm = document.querySelector('#new-whisky-form');
+    newWhiskyForm.addEventListener('submit', handleNewWhiskyFormSubmit);
+
+    const deleteAllButton = document.querySelector('#delete-all');
+    deleteAllButton.addEventListener('click', handleDeleteAllClick);
+});
+
+const handleNewWhiskyFormSubmit = function (event) {
+    event.preventDefault();
+
+    const whiskyListItem = createWhiskyListItem(event.target);
+    const whiskyList = document.querySelector('#whisky-list');
+    whiskyList.appendChild(whiskyListItem)
+};
+
+const createWhiskyListItem = function (form) {
+    const whiskyListItem = document.createElement('li');
+    whiskyListItem.classList.add('whisky-list-item');
+
+    const whisky = document.createElement('h3');
+    whisky.textContent = `Whisky: ${form.whisky.value}`;
+    whiskyListItem.appendChild(whisky);
+
+    const distillery = document.createElement('p');
+    distillery.textContent = `Distillery: ${form.distillery.value}`;
+    whiskyListItem.appendChild(distillery);
+
+    return whiskyListItem;
+};
+
+const handleDeleteAllClick = function (event) {
+    const whiskyList = document.querySelector('#whisky-list');
+    whiskyList.textContent = '';
+}
